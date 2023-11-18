fe1d0d29361b79aba99613ba83c2b985e3570d60
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-11-18 23:03:32 +0530

    added create table command

--- server/sql_statements.sql
+++ server/sql_statements.sql
@@ -1,3 +1,9 @@
-ALTER TABLE users
-ADD PRIMARY KEY (username);
+CREATE TABLE users (
+    uid SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE,
+    password VARCHAR(255),
+    email VARCHAR(255) UNIQUE
+);
 
+SELECT * FROM public.users
+ORDER BY uid ASC 
