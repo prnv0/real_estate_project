20c1db163d79b97d0bf972a641e708ebe4fc095b
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-11-19 12:26:48 +0530

    added create lisitng table command

--- server/sql_statements.sql
+++ server/sql_statements.sql
@@ -7,3 +7,19 @@ CREATE TABLE users (
 
 SELECT * FROM public.users
 ORDER BY uid ASC 
+
+CREATE TABLE listing (
+    property_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    type VARCHAR(50) CHECK(type IN ('house', 'villa', 'office', 'apartment')) NOT NULL,
+    price_per_sqft DECIMAL NOT NULL,
+    user_uid INT REFERENCES users(uid) ON DELETE CASCADE,
+    image_urls TEXT ARRAY DEFAULT '{}',
+    description TEXT DEFAULT '',
+    bedrooms INT DEFAULT 0,
+    bathrooms INT DEFAULT 0,
+    area_sqft INT DEFAULT 0,
+    year_built INT DEFAULT 0
+   
+);
