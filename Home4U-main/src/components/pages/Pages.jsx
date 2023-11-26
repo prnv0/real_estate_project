fbe02843624a9ebb0bfc0d7d45afeb72f0232417
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-11-26 18:56:19 +0530

    done bugs

--- Home4U-main/src/components/pages/Pages.jsx
+++ Home4U-main/src/components/pages/Pages.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from "react"
 import Header from "../common/header/Header"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
@@ -10,16 +10,7 @@ import Blog from "../blog/Blog"
 import Services from "../services/Services"
 import Contact from "../contact/Contact"
 import Login from "../login/login"
-=======
-import React from "react";
-import Header from "../common/header/Header";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "../home/Home";
-import Footer from "../common/footer/Footer";
-import About from "../about/About";
-import Services from "../services/Services";
-import Contact from "../contact/Contact";
->>>>>>> 16e100cf73b7da5d47caab3fb54b07058fc5127e
+
 
 const Pages = () => {
   return (
@@ -27,7 +18,6 @@ const Pages = () => {
       <Router>
         <Header />
         <Routes>
-<<<<<<< HEAD
           <Route exact path='/login' element={<Login />} />
           <Route path='/' element={<Home />} />
           <Route path="/about" element={<About />} />
@@ -35,12 +25,6 @@ const Pages = () => {
           <Route exact path='/blog' element={<Blog />} />
           <Route exact path='/pricing' element={<Pricing />} />
           <Route exact path='/contact' element={<Contact />} />
-=======
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route exact path="/services" element={<Services />} />
-          <Route exact path="/contact" element={<Contact />} />
->>>>>>> 16e100cf73b7da5d47caab3fb54b07058fc5127e
         </Routes>
         <Footer />
       </Router>
