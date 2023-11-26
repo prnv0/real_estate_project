752f416943fd56bd4f6b890f982a116d1100c093
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-11-26 18:57:39 +0530

    bugs fixed

--- Home4U-main/src/components/pages/Pages.jsx
+++ Home4U-main/src/components/pages/Pages.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
->>>>>>> a1d6a468055d852408502cf7e49e7edcd16dc362
 import React from "react"
 import Header from "../common/header/Header"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
@@ -14,20 +10,6 @@ import Blog from "../blog/Blog"
 import Services from "../services/Services"
 import Contact from "../contact/Contact"
 import Login from "../login/login"
-<<<<<<< HEAD
-
-=======
-=======
-import React from "react";
-import Header from "../common/header/Header";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "../home/Home";
-import Footer from "../common/footer/Footer";
-import About from "../about/About";
-import Services from "../services/Services";
-import Contact from "../contact/Contact";
->>>>>>> 16e100cf73b7da5d47caab3fb54b07058fc5127e
->>>>>>> a1d6a468055d852408502cf7e49e7edcd16dc362
 
 const Pages = () => {
   return (
@@ -35,10 +17,7 @@ const Pages = () => {
       <Router>
         <Header />
         <Routes>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> a1d6a468055d852408502cf7e49e7edcd16dc362
+
           <Route exact path='/login' element={<Login />} />
           <Route path='/' element={<Home />} />
           <Route path="/about" element={<About />} />
@@ -46,15 +25,6 @@ const Pages = () => {
           <Route exact path='/blog' element={<Blog />} />
           <Route exact path='/pricing' element={<Pricing />} />
           <Route exact path='/contact' element={<Contact />} />
-<<<<<<< HEAD
-=======
-=======
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route exact path="/services" element={<Services />} />
-          <Route exact path="/contact" element={<Contact />} />
->>>>>>> 16e100cf73b7da5d47caab3fb54b07058fc5127e
->>>>>>> a1d6a468055d852408502cf7e49e7edcd16dc362
         </Routes>
         <Footer />
       </Router>
